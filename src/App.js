b17e4becb4d2ed46779d34dc8ae5e481eeb97f8d
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-04-19 10:51:51 +0400

    adding new tasks to todo and adding status and handle the change of the status

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import './App.css';
+import {useState} from "react";
 
+
+function* uuidGen() {
+    let id = 0;
+
+    while (true) {
+        yield id;
+        id++
+    }
+}
+
+//trzeba zintancjonowac generator
+const uuid = uuidGen()
 
 function App() {
-  return (
-    <div className="App">
-      <h1>todos</h1>
-        <input type="text"/>
-    </div>
-  );
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleKeyUp = (event) => {
+        if (event.key === 'Enter') {
+            setTasks([...tasks, {
+                name: value,
+                id: uuid.next().value,
+                status: false
+            }])
+            setValue('');
+        }
+    }
+
+    function handleChangeStatus(id) {
+        //tworzymy nowa tablice
+        const newTasks = tasks.map((task) => {
+            if (task.id === id) {
+                task.status = !task.status
+            }
+            return task
+        })
+        setTasks((newTasks));
+    }
+
+    return (
+        <div className="App">
+            <h1>todos</h1>
+            <input type="text" value={value} onChange={handleChange} onKeyUp={handleKeyUp}/>
+            <p>{value}</p>
+            <ul>
+                {tasks.map(({id, name, status}) => (
+                    <li key={id} className='todo-item'>
+                        <span className={status ? 'status done' : 'status active'}
+                              onClick={() => handleChangeStatus(id)}/>
+                        {name}</li>))}
+            </ul>
+        </div>
+
+    );
 }
 
 export default App;
